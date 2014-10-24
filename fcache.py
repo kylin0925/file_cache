f77b6e0eba29be541bf21a9331cdd55d58fc70fd
kylin <kylin@appleteki-imac.(none)>
2014-10-24 20:26:57 +0800

    remove db_file in function argument. chanage conn to global

--- fcache.py
+++ fcache.py
@@ -3,50 +3,45 @@ import os
 import sql
 import sys
 cache_db = 'cache.db'
-def chk_table(dbfile,table):
-    execsql = "select * from sqlite_master where name='%s';" %(table)
-    conn = sql.db_connect(dbfile)
-    r=sql.db_sql(conn,execsql)
+conn = sql.db_connect(cache_db)
+
+def chk_table(table):
+    execsql = "select * from sqlite_master where name=?;"
+    r=sql.db_sql(conn,execsql,table)
     data = r.fetchall()
     #print data
-    sql.db_close(conn)
     return len(data)
 
-def is_dup(dbfile,fullpath,filename):
+def is_dup(fullpath,filename):
     fullpath = fullpath.replace("'","''")
     filename = filename.replace("'","''")
     execsql = "select * from file_list where fullpath='%s' and filename = '%s';" %(fullpath,filename)
-    print execsql
-    conn = sql.db_connect(dbfile)
+    #print execsql
     r=sql.db_sql(conn,execsql)
     data = r.fetchall()
     #print data
-    sql.db_close(conn)
     return len(data) > 0
 
 def create_table(dbfile):
-    global cache_db
-    conn = sql.db_connect(dbfile)
+    #global cache_db
     r = sql.db_sql(conn,"create table file_list (fullpath text,filename text)")
-    sql.db_close(conn)
-
 
 def chk_db(dbfile):
     if os.access(dbfile,os.R_OK): 
-        if chk_table(cache_db,'file_list') == 0:
+        if chk_table('file_list') == 0:
             create_table(dbfile)
     else:
         create_table(dbfile)
     
 def start_cache():
     sdir = '/Users/kylin/Downloads'
-    conn = sql.db_connect(cache_db)
+    #conn = sql.db_connect(cache_db)
     cnt =0;
     print "start file cache"
     for dname,dnames,files in os.walk(sdir):
         for f in files:
             full_path = dname + "/"+ f
-            if is_dup(cache_db,full_path,f) == False:
+            if is_dup(full_path,f) == False:
                 #print  full_path
                 execsql = 'insert into file_list (fullpath,filename) values ("%s","%s")' % (full_path,f)
                 #print execsql
@@ -58,21 +53,22 @@ def start_cache():
             else:
                 sys.stdout.write('cached file count :' + str(cnt) + ' dup skip' + '\r')
                 sys.stdout.flush()
-    sql.db_close(conn)
-def query_ext(dbfile,ext):
+    print ""
+def query_ext(ext):
     #execsql='''select fullpath,filename from file_list where filename like "%.%s"
     #         group by filename having(COUNT(*)>15);'''
     
     execsql='select fullpath,filename from file_list where filename like "%.%s"'
-    conn = sql.db_connect(dbfile)
     r=sql.db_sql(conn,execsql)
     data = r.fetchall()
     #print data
-    sql.db_close(conn)
     for f in data:
         print f[0].encode('utf-8'),f[1].encode('utf-8')
 
-#chk_db(cache_db)
-#start_cache()
+chk_db(cache_db)
+start_cache()
 #print is_dup(cache_db,'test','test')
-query_ext(cache_db,'jpg')
+#query_ext('jpg')
+
+#close    
+sql.db_close(conn)
