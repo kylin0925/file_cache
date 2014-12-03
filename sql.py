6855d90de41a7c69b832f5c24dfdc82fda56c4cb
kylin <kylin@gmail.com>
2014-12-03 22:55:54 +0800

    update sqlite

--- sql.py
+++ sql.py
@@ -1,27 +1,32 @@
 import sqlite3
-def db_connect(db_file):
-    conn = sqlite3.connect(db_file)
-    return conn
-def db_sql(conn,sql):
-    c = conn.cursor()
-    row = c.execute(sql)
-    #c.execute("create table test (a text ,b text)")
-    conn.commit()
-    return row
-def db_sql(conn,sql,*arg):
-    c = conn.cursor()
-    row = c.execute(sql,arg)
-    #c.execute("create table test (a text ,b text)")
-    conn.commit()
-    return row
+class db:
+    conn = None
+    def db_connect(self,db_file):
+        self.conn = sqlite3.connect(db_file)
+        #return conn
+    def db_sql(self,sql):
+        c = self.conn.cursor()
+        row = c.execute(sql)
+        #c.execute("create table test (a text ,b text)")
+        self.conn.commit()
+        return row
+    def db_sql(self,sql,*arg):
+        c = self.conn.cursor()
+        row = c.execute(sql,arg)
+        #c.execute("create table test (a text ,b text)")
+        self.conn.commit()
+        return row
 
-def db_close(conn):
-    conn.close()
-'''
-conn = db_connect('test.db')
-row = db_sql(conn,'select * from test')
-for d in row:
-    print row
-db_close(conn)
-'''
+    def db_close(self):
+        self.conn.close()
+
+def test():
+    foodb = db()
+    foodb.db_connect('test.db')
+    row = foodb.db_sql('select * from sqlite_master')
+    for d in row:
+        print d
+    foodb.db_close()
+if __name__ == "__main__":
+    test()
 
